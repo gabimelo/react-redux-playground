767093e4e0e323f70663c5cb81db35974212b9f7
Gabriela Melo <gabimelo10@gmail.com>
2017-03-26 17:20:29 -0300

    added YT API

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
+const API_KEY = 'AIzaSyCAm_vQV6y6ANh4f9MdeCR4EwX-yE6l_tI';
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  // <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  </Provider>
+  // </Provider>
   , document.querySelector('.container'));
