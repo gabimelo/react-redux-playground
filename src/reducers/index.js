f8122d08828b48f3ba1aafeb267cfd6e313d2e08
Gabriela Melo <gabimelo10@gmail.com>
2017-03-27 15:49:26 -0300

    section 3 done

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
+import ActiveBookReducer from './reducer_active_book';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+	// these are the keys to global state
+	books: BooksReducer,
+	activeBook: ActiveBookReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{title: 'JS', pages: 10},
+		{title: 'HP', pages: 20},
+		{title: 'TDT', pages: 30},
+		{title: 'ER', pages: 40}
+	]
+}
