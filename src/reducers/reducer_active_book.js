45c9fc7dd79dc830b7783b1bade45d2eb169c5a6
Gabriela Melo <gabimelo10@gmail.com>
2017-05-24 00:11:35 -0300

    update

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,9 +1,10 @@
 export default function(state = null, action) {
 	switch(action.type) {
-	case 'BOOK_SELECTED':
-		return action.payload;
+		case 'BOOK_SELECTED':
+			return action.payload;
+		default:
+			// when we dont care about the action:
+			return state;
 	}
 
-	// when we dont care about the action:
-	return state;
 }
